54e766ab2a76b843e32efe268b87a7c62b010f17
Hiba-Almade <hibaalmade998@gmail.com>
2021-10-30 21:03:14 +0200

    add test

--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.songr;
 
+import com.example.songr.models.Album;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,12 @@ class SongrApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void albumTester() {
+		Album album = new Album("test", "artist 1", 6, 33, "test");
+		Assertions.assertEquals(album.getTitle(), "test");
 	}
 
 }
